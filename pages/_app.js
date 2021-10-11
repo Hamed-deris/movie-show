1118ad8cc0c2dd491be55c2a9bf227b651585c9b
unknown <hemed.deris.dev@gmail.com>
2021-10-11 08:23:04 -0700

    remove screen size

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,11 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 
 const Layout = dynamic(() => import("../components/layout/Layout"));
-const ScreenSize = dynamic(() => import("../components/hooks/ScreenSize"));
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <ScreenSize />
     </Layout>
   );
 }
